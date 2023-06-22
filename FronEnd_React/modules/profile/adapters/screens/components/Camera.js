ae5f845b88c4778d0562a7c339b33cebcba25188
andr3sEnrique <20213tn080@utez.edu.mx>
2023-06-22 10:39:13 -0600

    consumo api

--- FronEnd_React/modules/profile/adapters/screens/components/Camera.js
+++ FronEnd_React/modules/profile/adapters/screens/components/Camera.js
@@ -1,6 +1,7 @@
 import Loader from '../../../../../kernel/components/Loading';
 import React, { useState, useEffect, useContext } from 'react';
 import { Text, View, StyleSheet, TouchableOpacity, Image } from 'react-native';
+
 import { Camera } from 'expo-camera';
 import { useNavigation } from "@react-navigation/native";
 
@@ -8,6 +9,7 @@ export default function CameraComponent  ({  isVisible, onClose, onPictureTaken 
   const [hasPermission, setHasPermission] = useState(null);
   const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
   const [isPreview, setIsPreview] = useState(false);
+  const [photo, setPhoto] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   let camera = null;
 
@@ -18,9 +20,61 @@ export default function CameraComponent  ({  isVisible, onClose, onPictureTaken 
     })();
   }, []);
 
-  const handleClose = () => {
-    onClose();
+
+  const takePicture = async () => {
+    setIsLoading(true);
+    if (camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await camera.takePictureAsync(options);
+      setPhoto(data.uri);
+      setIsPreview(true);
+      // Crear objeto FormData para enviar la imagen a la API
+      const formData = new FormData();
+      formData.append('file', {
+        uri: data.uri,
+        name: 'photo.jpg',
+        type: 'image/jpeg'
+      });
+  
+      try {
+        const response = await fetch('http://127.0.0.1:5000/detected_img', {
+          method: 'POST',
+          body: formData
+        });
+  
+        if (response.ok) {
+          // La imagen se envió correctamente a la API
+  
+          // Aquí puedes realizar las acciones adicionales que deseas después de enviar la imagen
+  
+        } else {
+          // Ocurrió un error al enviar la imagen a la API
+          console.log('Error al enviar la imagen a la API');
+        }
+      } catch (error) {
+        console.log('Error de red:', error);
+      }
+  
+      
+  
+      // Resto del código de la función...
+    }
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
   };
+  
+  const closeCamera = () => {
+    setIsPreview(false); // Cierra la vista previa de la imagen
+    setPhoto(null); // Limpia la imagen capturada
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
 
   return (
     <View style={styles.container}>
@@ -34,9 +88,15 @@ export default function CameraComponent  ({  isVisible, onClose, onPictureTaken 
           }}
         >
           <View style={styles.buttonContainer}>
+          <TouchableOpacity
+              style={styles.button}
+              onPress={takePicture}
+            >
+              <Text style={styles.text}>Tomar Foto</Text>
+            </TouchableOpacity>
             <TouchableOpacity
               style={styles.button}
-              onPress={handleClose}
+              onPress={closeCamera}
             >
               <Text style={styles.text}>Cerrar</Text>
             </TouchableOpacity>
